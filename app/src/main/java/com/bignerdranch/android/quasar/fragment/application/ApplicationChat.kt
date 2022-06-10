8276caf39623b709b41d8b85a18027387c0cebd1
alihashkina <85725433+alihashkina@users.noreply.github.com>
2022-06-10 18:22:46 +0300

    .

--- app/src/main/java/com/bignerdranch/android/quasar/fragment/application/ApplicationChat.kt
+++ app/src/main/java/com/bignerdranch/android/quasar/fragment/application/ApplicationChat.kt
@@ -2,8 +2,15 @@ package com.bignerdranch.android.quasar.fragment.application
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,28 +18,27 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bignerdranch.android.quasar.MainActivity
 import com.bignerdranch.android.quasar.R
 import com.bignerdranch.android.quasar.databinding.ApplicationChatFragmentBinding
-import com.bignerdranch.android.quasar.databinding.PhotoItemBinding
+import com.bignerdranch.android.quasar.fragment.equipment.Equipment
 import com.bignerdranch.android.quasar.ui.viewmodel.application.ApplicationChatViewModel
 import com.bignerdranch.android.quasar.ui.viewmodel.application.ApplicationChatViewModel.Companion.msgTrueFale
 import com.bignerdranch.android.quasar.ui.viewmodel.application.PhotoAdapter
-import android.R.attr.data
-
-
-
+import java.io.File
 
 class ApplicationChat : Fragment() {
 
     companion object {
         fun newInstance() = ApplicationChat()
-        val REQUEST_CODE = 100
+        var PICK_IMAGE_MULTIPLE = 3
+        val REQUEST_CODE = 3
+        var photoUri: ArrayList<Uri> = ArrayList()
         var photoImg: ArrayList<ImageView> = ArrayList()
     }
-
 
     private lateinit var viewModel: ApplicationChatViewModel
     lateinit var bindingApplicationChat: ApplicationChatFragmentBinding
@@ -58,26 +64,24 @@ class ApplicationChat : Fragment() {
 
 
 bindingApplicationChat.imgApplicationChatMsgPhoto.setOnClickListener{
-    getPhoto()
+   getPhoto()
 }
-
-    }
+        }
     fun getPhoto() {
     val intent = Intent(Intent.ACTION_PICK)
     intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
     intent.type = "image/*"
-        startActivityForResult(intent, REQUEST_CODE)
-    }
+    startActivityForResult(
+        Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE
+    )
 
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-     data?.extras
+        //не работает
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
             bindingApplicationChat.linearPhoto.visibility = VISIBLE
-           // bindingApplicationChat.imageView2.setImageURI(data?.data)
-            //не работает
-            photoImg = arrayListOf(PhotoItemBinding.bind(view!!).imgPhoto)
           photoImg[0].setImageURI(data?.data)
             photoImg[1].setImageURI(data?.data)
             photoImg[2].setImageURI(data?.data)
